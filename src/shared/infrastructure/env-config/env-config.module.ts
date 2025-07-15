a311ee6456346dc09ffe0e7261736662e5d68035
Pedro Henrique <pedrohenriquedasilvapereira12@gmail.com>
2025-07-14 22:38:28 -0300

    Enhance EnvConfigModule to extend ConfigModule and support dynamic environment file loading based on NODE_ENV

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,20 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { join } from 'path';
 import { EnvConfigService } from './env-config.service';
 
 @Module({
   providers: [EnvConfigService],
 })
-export class EnvConfigModule {}
+export class EnvConfigModule extends ConfigModule {
+  static async forRoot(
+    options: ConfigModuleOptions = {},
+  ): Promise<DynamicModule> {
+    return super.forRoot({
+      ...options,
+      envFilePath: [
+        join(__dirname, `../../../../../.env.${process.env.NODE_ENV}`),
+      ],
+    });
+  }
+}
